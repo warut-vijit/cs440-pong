131ddbf7b0bb0bc910ddd538b0e911ec954a452a
Duke Vijitbenjaronk <warut.vijitbenjaronk@gmail.com>
2017-02-08 21:05:53 -0600

    things now call other things

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import MDP.MDP as MDP
+
 
 class Simulator:
     
@@ -16,15 +18,14 @@ class Simulator:
         self.gamma_val = gamma_value
         self.q_array = [[0] * 10369]*3 # 2d array of q values, length 10369, with 3 actions 
         
-        # Your Code Goes Here
+        self.train_agent()
     
     def f_function(self, state, q):
         '''
         Choose action based on an epsilon greedy approach
         :return action selected
         '''
-        action_selected = None
-        
+        action_selected = 0
         # Your Code Goes Here!
         
         return action_selected
@@ -33,7 +34,7 @@ class Simulator:
         '''
         Train the agent over a certain number of games.
         '''
-        # Your Code Goes Here!
+        self.play_game()
         pass
     
     def play_game(self):
@@ -42,9 +43,9 @@ class Simulator:
         '''
         #apply rewards and stuff
         game_log = []
-        game = MDP(ball_x=0.5,ball_y=0.5,velocity_x=0.03,velocity_y=0.01,paddle_y=0.4,q_array=q_array)
+        game = MDP(ball_x=0.5,ball_y=0.5,velocity_x=0.03,velocity_y=0.01,paddle_y=0.4,q_array=self.q_array)
         while True: # while paddle has not missed
-            action = f_function(game.discretize_state, game.q_array) # get action given current state, q array
+            action = self.f_function(game.discretize_state, game.q_array) # get action given current state, q array
             game.simulate_one_time_step(action) # advance one step
             if game.bounce: # if paddle bounced
                 game_log.append( (game.discretize_state, action, 1) )
